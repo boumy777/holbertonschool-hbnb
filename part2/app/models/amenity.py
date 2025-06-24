aad3af9a4cf5b32f4e3a539b43635708566880e1
boumy777 <brandondialuta@gmail.com>
2025-06-25 01:53:59 +0200

    amenity changement

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,8 +1,9 @@
-# business/models/amenity.py
+# app/models/amenity.py
 
 from app.models.base_model import BaseModel
 
 class Amenity(BaseModel):
-    def __init__(self, *args, **kwargs):
-        self.name = ""
-        super().__init__(*args, **kwargs)
+    def __init__(self, name="", id=None, created_at=None, updated_at=None):
+        super().__init__(id, created_at, updated_at)
+        self.name = name
+
